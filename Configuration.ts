f5da0153cddcad7098e95470c677e3184de215f1
Abthakur1 <absk.cts@gmail.com>
2020-08-22 19:39:32 +0530

    Added Cucumber html report generation

--- Configuration.ts
+++ Configuration.ts
@@ -1,5 +1,5 @@
 import { Config, Capabilities } from "protractor";
-
+import * as reporter from "cucumber-html-reporter"
 
 export let config: Config = {
     directConnect: true,
@@ -13,6 +13,54 @@ export let config: Config = {
     specs: ['../Features/Test1.feature'],
 
     cucumberOpts: {
-        require: 'Features/Step_Definitions/StepDef.js'
-    }
-};
+        // require: 'Features/Step_Definitions/StepDef.js',
+        format: 'json:./cucumberreports.json',
+        // format: ['progress', 'pretty:output.txt'],
+        //format:'pretty:./cucumberreports2.txt',
+
+        onComplete: () => {
+            //var reporter = require('cucumber-html-reporter');
+
+            var options = {
+                theme: 'bootstrap',
+                jsonFile: './cucumberreports.json',
+                output: './cucumber_report.html',
+                reportSuiteAsScenarios: true,
+                scenarioTimestamp: true,
+                launchReport: true,
+                metadata: {
+                    "App Version": "0.3.2",
+                    "Test Environment": "STAGING",
+                    "Browser": "Chrome  54.0.2840.98",
+                    "Platform": "Windows 10",
+                    "Parallel": "Scenarios",
+                    "Executed": "Remote"
+                }
+            };
+
+            reporter.generate(options);
+
+        },
+        require: [
+            'Features/Step_Definitions/StepDef.js'
+        ]
+    },
+    plugins: [{
+        package: require.resolve('protractor-multiple-cucumber-html-reporter-plugin'),
+        options: {
+            automaticallyGenerateReport: true,
+            removeExistingJsonReportFile: true,
+            openReportInBrowser: true,
+            pageTitle: "Project Report",
+            pageFooter: "<div><p>Protractor with cucumber</p></div>",
+            customData: {
+                title: 'Protractor Cucucmber Report',
+                data: [
+                    { label: 'Project', value: 'Protractor with Cucumber test' },
+                    { label: 'Created By', value: 'Abhishek Thakur' }
+                ]
+            }
+        },
+
+    }]
+}
